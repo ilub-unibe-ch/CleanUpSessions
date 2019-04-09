ca63053c2cbb77440132a69f730d94f9c4e58e9f
Olivia Kaufmann <olivia.kaufmann@ilub.unibe.ch>
2019-04-09 09:43:38 +0200

    rename log file

--- classes/class.ilCleanUpSessionsPlugin.php
+++ classes/class.ilCleanUpSessionsPlugin.php
@@ -19,7 +19,7 @@ class ilCleanUpSessionsPlugin extends ilCronHookPlugin {
 	const DEFAULT_EXPIRATION_VALUE = 240;
 	const EXPIRATION_THRESHOLD = 'expiration_threshold';
 	# const IL_PLUGIN_TABLE = 'il_plugin';
-	const LOG_DESTINATION = '/var/log/ilias/app.log';
+	const LOG_DESTINATION = '/var/log/ilias/CleanUpSessions.log';
 
 	/**
 	 * @var ilCleanUpSessionsPlugin
